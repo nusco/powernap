c99d98cc9ad0343c734868f2e573ef7832cb4a4d
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2011-04-21 19:21:31 +0200

    fixed refactoring

--- lib/powernap.rb
+++ lib/powernap.rb
@@ -2,5 +2,5 @@ require_relative 'powernap/configuration'
 require_relative 'powernap/application'
 require_relative 'powernap/resource'
 
-require_relative 'powernap/mongoid_resource'
+require_relative 'powernap/resource/mongoid'
 PowerNap::PersistentResource = PowerNap::MongoidResource
