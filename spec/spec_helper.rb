9026a167a0dc815a6f62e0475e032ce84f6faa12
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2011-04-26 17:07:08 +0200

    Moved instructions to start mongo to production code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,5 @@
 require 'bundler/setup'
 
-require 'mongoid'
-require 'rspec'
-
-require 'mongo'
-begin
-  Mongo::Connection.new('localhost', 27017).close
-rescue Mongo::ConnectionFailure
-  puts "                                                                  "
-  puts "===================== Hey, where is MongoDB? ====================="
-  puts "To run these tests, I need a Mongo server running on port 27017.  "
-  puts "Start a server with the 'mongod' command.                         "
-  puts "                                                                  "
-  puts "If you don't have MongoDB yet, you can install it via Homebrew    "
-  puts "(type 'brew install mongo'). If you don't have Homebrew, read     "
-  puts "the quickstart at http://www.mongodb.org/display/DOCS/Quickstart. "
-  puts "=================================================================="
-  exit
-end
-
 require 'rack/test'
 ENV['RACK_ENV'] = 'test'
 
